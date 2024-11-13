6f75c9a0dc4ed73d596dae78d0d84853a8f1f095
JunaYa <ayac3j@gmail.com>
2024-11-13 23:56:06 +0800

    feat: set title bar transparent

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,6 @@
-use tauri::tray::TrayIconBuilder;
+use tauri::{
+    tray::TrayIconBuilder, window::Color, TitleBarStyle, WebviewUrl, WebviewWindowBuilder,
+};
 
 mod menu;
 mod screenshot;
@@ -12,36 +14,17 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_clipboard_manager::init())
         .setup(|app| {
             #[cfg(desktop)]
-            {
-                use tauri_plugin_autostart::MacosLauncher;
-                use tauri_plugin_autostart::ManagerExt;
+            configure_autostart(app);
 
-                app.handle().plugin(tauri_plugin_autostart::init(
-                    MacosLauncher::LaunchAgent,
-                    Some(vec!["--flag1", "--flag2"]),
-                ));
+            create_window(app)?;
 
-                // Get the autostart manager
-                let autostart_manager = app.autolaunch();
-                // Enable autostart
-                let _ = autostart_manager.enable();
-            }
-
-            // setting tray
-            let _ = TrayIconBuilder::with_id("main-tray")
-                .menu(&menu::get_tray_menu(app.handle())?)
-                .icon(app.default_window_icon().unwrap().clone())
-                .icon_as_template(true)
-                .menu_on_left_click(true)
-                .on_menu_event(menu::handle_tray_menu_events)
-                .on_tray_icon_event(menu::handle_tray_icon_events)
-                .build(app)?;
+            create_tray(app)?;
 
             Ok(())
         })
+        .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
@@ -56,4 +39,63 @@ pub fn run() {
         .menu(menu::get_app_menu)
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+#[cfg(desktop)]
+fn configure_autostart(app: &tauri::App) {
+    use tauri_plugin_autostart::MacosLauncher;
+    use tauri_plugin_autostart::ManagerExt;
+
+    let _ = app.handle().plugin(tauri_plugin_autostart::init(
+        MacosLauncher::LaunchAgent,
+        Some(vec!["--flag1", "--flag2"]),
+    ));
+
+    // Get the autostart manager
+    let autostart_manager = app.autolaunch();
+    // Enable autostart
+    let _ = autostart_manager.enable();
+}
+
+fn create_window(app: &mut tauri::App) -> Result<(), tauri::Error> {
+    let win_builder = WebviewWindowBuilder::new(app, "main", WebviewUrl::default())
+        .title("ddu")
+        .title_bar_style(TitleBarStyle::Transparent)
+        .background_color(Color(10, 100, 100, 1))
+        .inner_size(800.0, 600.0);
+
+    let window = win_builder.build().unwrap();
+
+    // set background color only when building for macOS
+    #[cfg(target_os = "macos")]
+    {
+        use cocoa::appkit::{NSColor, NSWindow};
+        use cocoa::base::{id, nil};
+
+        let ns_window = window.ns_window().unwrap() as id;
+        unsafe {
+            let bg_color = NSColor::colorWithRed_green_blue_alpha_(
+                nil,
+                50.0 / 255.0,
+                158.0 / 255.0,
+                163.5 / 255.0,
+                0.5,
+            );
+            ns_window.setBackgroundColor_(bg_color);
+        }
+    }
+
+    Ok(())
+}
+
+fn create_tray(app: &mut tauri::App) -> Result<(), tauri::Error> {
+    let _ = TrayIconBuilder::with_id("main-tray")
+        .menu(&menu::get_tray_menu(app.handle())?)
+        .icon(app.default_window_icon().unwrap().clone())
+        .icon_as_template(true)
+        .menu_on_left_click(true)
+        .on_menu_event(menu::handle_tray_menu_events)
+        .on_tray_icon_event(menu::handle_tray_icon_events)
+        .build(app)?;
+    Ok(())
 }
--- src/App.vue
+++ src/App.vue
@@ -8,22 +8,6 @@ import AutoStart from "./components/AutoStart.vue";
 
 <template>
   <div class="container">
-    <h1>Welcome to Tauri!</h1>
-
-    <div class="row">
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-      </a>
-      <a href="https://tauri.app" target="_blank">
-        <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
-    </div>
-
-    <p>Click on the Tauri, Vite, and Vue logos to learn more.</p>
-
     <AutoStart />
     <Greet />
     <Screenshot />
@@ -31,14 +15,6 @@ import AutoStart from "./components/AutoStart.vue";
 </template>
 
 <style scoped>
-.logo.vite:hover {
-  filter: drop-shadow(0 0 2em #747bff);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #249b73);
-}
-
 :root {
   font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
   font-size: 16px;
@@ -46,8 +22,7 @@ import AutoStart from "./components/AutoStart.vue";
   font-weight: 400;
 
   color: #0f0f0f;
-  background-color: #f6f6f6;
-
+  background-color: #FFFFFF;
   font-synthesis: none;
   text-rendering: optimizeLegibility;
   -webkit-font-smoothing: antialiased;
@@ -61,36 +36,6 @@ import AutoStart from "./components/AutoStart.vue";
   display: flex;
   flex-direction: column;
   justify-content: center;
-  text-align: center;
-}
-
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: 0.75s;
-}
-
-.logo.tauri:hover {
-  filter: drop-shadow(0 0 2em #24c8db);
-}
-
-.row {
-  display: flex;
-  justify-content: center;
-}
-
-a {
-  font-weight: 500;
-  color: #646cff;
-  text-decoration: inherit;
-}
-
-a:hover {
-  color: #535bf2;
-}
-
-h1 {
   text-align: center;
 }
 
