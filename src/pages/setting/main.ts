454983c788a0c85ffeb2fd7f19d5bb24072ce258
JunaYa <ayac3j@gmail.com>
2024-11-15 16:42:58 +0800

    chore: add unocs

--- src/pages/setting/main.ts
+++ src/pages/setting/main.ts
@@ -2,5 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import '~/styles/vars.css'
 import '~/styles/global.css'
-
+import 'virtual:uno.css'
 createApp(App).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import path from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import UnoCSS from 'unocss/vite'
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [vue()],
+  plugins: [vue(),UnoCSS()],
 
   resolve: {
     alias: {
