162a28be9ca2e72aa5891dfc3162e439e6e9e087
JunaYa <ayac3j@gmail.com>
2024-11-19 16:09:05 +0800

    fix: vue funciton use error

--- src/components/SnapVaultItem.vue
+++ src/components/SnapVaultItem.vue
@@ -2,24 +2,26 @@
 import { confirm } from '@tauri-apps/plugin-dialog'
 import { remove } from '@tauri-apps/plugin-fs'
 import { useElementHover } from '@vueuse/core'
-import { ref } from 'vue'
+import { defineEmits, defineProps, ref } from 'vue'
 import Button from '~/components/Button.vue'
 import FileInfo from './FileInfo.vue'
 import PictureReview from './PictureReview.vue'
 
 defineProps<{
   item: { id: string, image: string }
-  change?: () => void
 }>()
+
 const emit = defineEmits<{
   (e: 'change'): void
 }>()
+
 const snapHoverableElement = ref()
 const isHovered = useElementHover(snapHoverableElement)
 
 const deleteLoading = ref(false)
 
 async function handleDelete(path: string) {
+  console.log(path)
   deleteLoading.value = true
   const confirmation = await confirm(
     '是否确认删除该文件?',
@@ -34,11 +36,10 @@ async function handleDelete(path: string) {
 </script>
 
 <template>
-  <div ref="snapHoverableElement" class="relative rounded-md bg-card p-2">
+<div ref="snapHoverableElement" class="relative rounded-md bg-card p-2">
     <PictureReview :image-path="item.image" />
-    <!-- button group delete | rename | copy -->
     <div v-if="isHovered" class="absolute right-3 top-3 z-11 flex flex-row gap-2">
-      <Button class="btn-action-icon" anim @click="handleDelete(item.image)">
+      <Button class="btn-action-icon" anim @click="()=>handleDelete(item.image)">
         <i class="h-4 w-4">
           <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32"><path d="M12 12h2v12h-2z" fill="currentColor" /><path d="M18 12h2v12h-2z" fill="currentColor" /><path d="M4 6v2h2v20a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8h2V6zm4 22V8h16v20z" fill="currentColor" /><path d="M12 2h8v2h-8z" fill="currentColor" /></svg>
         </i>
