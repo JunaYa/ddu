65596bca48d1f42647615600b006c1bece485784
Arjun <arjun@Arjuns-Mac-mini.local>
2024-12-06 23:40:41 +0800

    chore: reset home layout

--- src/pages/main/App.vue
+++ src/pages/main/App.vue
@@ -1,15 +1,12 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Screenshot from '~/components/Screenshot.vue'
+import SnapVault from '~/components/SnapVault.vue'
 </script>
 
 <template>
   <div class="p-4">
-    <div class="mb-2 text-lg font-bold">
-      Screenshot
-    </div>
-    <Screenshot />
+    <SnapVault />
   </div>
 </template>
 
