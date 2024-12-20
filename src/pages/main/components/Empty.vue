2e285e7f1703649fdff915319500887b8e6baa4a
Arjun <ayac3j@gmail.com>
2024-12-21 01:56:29 +0800

    feat: support empty view

---
+++ src/pages/main/components/Empty.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import empty from '~/assets/images/empty.png'
+</script>
+
+<template>
+  <div class="m-auto h-full w-full flex flex-col items-center justify-center">
+    <img :src="empty" alt="empty" class="h-[430px] w-[340px]">
+  </div>
+</template>
